82cff8a6af16d69083ff6c2804331a9293f3ef88
tnakagawa <nakagat@gmail.com>
2021-09-05 10:13:50 +0900

    update ecdsarecover script.js

--- ecdsarecover/script.js
+++ ecdsarecover/script.js
@@ -310,7 +310,7 @@ function pow(x, y, p) {
     let z = 1n;
     while (y > 0n) {
         if (y & 1n) {
-            z = z * x % p;
+            z = (z * x) % p;
         }
         x = (x * x) % p;
         y >>= 1n;
